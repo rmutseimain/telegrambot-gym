41af886072773b54afce29105bc47bc9a6858351
rmutseimain <147353918+rmutseimain@users.noreply.github.com>
2023-12-29 17:00:22 +0200

    added dabase

--- main/db/init_database.sql
+++ main/db/init_database.sql
@@ -1,13 +1,15 @@
-CREATE TABLE IF NOT EXISTS "users" (
+CREATE TABLE IF NOT EXISTS "surveys" (
     "id" SERIAL,
-    "name" VARCHAR(100) NOT NULL,
-    "role" VARCHAR(15) NOT NULL,
+    "name" VARCHAR(50) NOT NULL,
+    "description" VARCHAR(300) NOT NULL,
+    "date" VARCHAR(100) NOT NULL,
     PRIMARY KEY ("id")
 );
 
-CREATE TABLE IF NOT EXISTS "survey" (
+CREATE TABLE IF NOT EXISTS "surveys_results" (
     "id" SERIAL,
-    "name" VARCHAR(100) NOT NULL,
-    "role" VARCHAR(15) NOT NULL,
+    "name" VARCHAR(50) NOT NULL,
+    "results" VARCHAR(300) NOT NULL,
+    "date" VARCHAR(100) NOT NULL,
     PRIMARY KEY ("id")
 );
